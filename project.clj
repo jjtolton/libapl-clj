43e83870dec49474554477e055fd29d57f636368
Jay <jjtolton@gmail.com>
2021-01-02 18:11:07 -0500

    add project.clj for clojars deploy

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jjtolton/libapl-clj "0.0.1-ALPHA"
+  :description "Clojure bindings for GNU APL"
+  :url "http://github.com/jjtolton/libapl-clj"
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
